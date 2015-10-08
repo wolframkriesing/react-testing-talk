f6b61cc5d8e62188ef014c7c2999de501784b032
Wolfram Kriesing <wk@uxebu.com>
2015-10-08 17:01:35 +0200

    Simulate a click on a number.

--- test/calculator-spec.js
+++ test/calculator-spec.js
@@ -7,23 +7,44 @@ import './setup-jsdom';
 
 describe('calculator example', function() {
 
-  it('initially the screen is empty', function() {
-    class Calculator extends React.Component { 
-      render() { 
-        return (
-          <div>
-            <div>0</div>
-            <button>1</button>
-          </div>
-        )
-      }
+  class Calculator extends React.Component {
+    constructor() {
+      super();
+      this.state = {screen: 0};
     }
-
+    render() { 
+      const click = () => this.setState({screen: 1});
+      return (
+        <div>
+          <div>{this.state.screen}</div>
+          <button onClick={click}>1</button>
+        </div>
+      )
+    }
+  }
+  
+  it('initially the screen is empty', function() {
     const rendered = TestUtils.renderIntoDocument(<Calculator />);
     const divs = TestUtils.scryRenderedDOMComponentsWithTag(rendered, 'div');
     
     const screenContent = divs[1].innerHTML;
     assert.equal(screenContent, '0');
+  });
+
+  it('a click on 1 renders 1 onto the screen', function() {
+
+    function clickButton(rendered) {
+      const button = TestUtils.findRenderedDOMComponentWithTag(rendered, 'button');
+      const buttonNode = ReactDOM.findDOMNode(button);
+      TestUtils.Simulate.click(buttonNode);
+    }
+    
+    const rendered = TestUtils.renderIntoDocument(<Calculator />);
+    const divs = TestUtils.scryRenderedDOMComponentsWithTag(rendered, 'div');
+    clickButton(rendered);
+    
+    const screenContent = divs[1].innerHTML;
+    assert.equal(screenContent, '1');
   });
   
 });
