c5cda531b8e7c91e0eb189d9d584c289efc8cc63
Wolfram Kriesing <wk@uxebu.com>
2015-10-08 16:53:51 +0200

    Make the test work and run with jsdom.

--- test/calculator-spec.js
+++ test/calculator-spec.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
 import assert from 'assert';
+
+import './setup-jsdom';
 
 describe('calculator example', function() {
 
   it('initially the screen is empty', function() {
-    let calculator = (
-      <div>
-        <div></div>
-        <button>1</button>
-      </div>
-    );
+    class Calculator extends React.Component { 
+      render() { 
+        return (
+          <div>
+            <div>0</div>
+            <button>1</button>
+          </div>
+        )
+      }
+    }
+
+    const rendered = TestUtils.renderIntoDocument(<Calculator />);
+    const divs = TestUtils.scryRenderedDOMComponentsWithTag(rendered, 'div');
     
-    // render the calculator
-    
-    const screenContent = null;
-    assert.equal(screenContent, '');
+    const screenContent = divs[1].innerHTML;
+    assert.equal(screenContent, '0');
   });
   
 });
